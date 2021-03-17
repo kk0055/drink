f420cdc26d177de5fbf29d889fa17dac5b0eebc5
kk0055 <rtsw.lgy@gmail.com>
2021-03-17 22:57:32 +0800

    favorite count

--- resources/views/drinks/my_favorites.blade.php
+++ resources/views/drinks/my_favorites.blade.php
@@ -2,7 +2,7 @@
 @section('contents')
 @include('nav')
 <div class="search-text flex flex-col w-full items-center  px-6 tracking-wide">
-  <h1> <i class="fas fa-star text-yellow-300"></i>俺のお気に入り<i class="fas fa-star text-yellow-300"></i></h1>
+  <h1> <i class="fas fa-star text-yellow-300"></i>俺の推し<i class="fas fa-star text-yellow-300"></i></h1>
  
   </div>
 
