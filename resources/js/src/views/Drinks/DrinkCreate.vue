e0e7b7837a6f955fc38271544b02831be2b5f8d9
kk0055 <rtsw.lgy@gmail.com>
2022-12-11 17:09:17 +0900

    list fix

--- resources/js/src/views/Drinks/DrinkCreate.vue
+++ resources/js/src/views/Drinks/DrinkCreate.vue
@@ -66,7 +66,6 @@
                         >
                         <select
                             v-model="data.prefecture"
-                            :id="prefectures"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             @input="$v.data.prefecture.$touch()"
                         >
