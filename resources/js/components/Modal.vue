04456de8f74972f29eea3b75ff7de4f4aa567b4c
kk0055 <rtsw.lgy@gmail.com>
2022-12-11 17:48:25 +0900

    海外追加

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -33,7 +33,7 @@
                                 :for="item.id"
                                 class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900 my-1"
                             >
-                                #{{ item.name }}
+                                {{ item.name }}
                             </label>
                             <!-- <div
                                 class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900 my-1"
